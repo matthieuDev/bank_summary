9f18694edf00991277596ae76212314d2ce1bc15
matthieuDev <matthieu.devaux.epfl@outlook.com>
2024-09-28 20:57:23 +0200

    load_from_creditmutuel_pandas

--- creditmutuel_load_transactions.py
+++ creditmutuel_load_transactions.py
@@ -13,3 +13,23 @@ def load_from_creditmutuel_file(filename, cat_column="Catégorie"):
     df["date"] = df["date"].map(lambda x : x.replace('/', ' '))
     
     return df.to_dict('records')
+
+def date2month(date) :
+    return '/'.join(date.split('/')[1:])
+
+def load_from_creditmutuel_pandas(filename,
+    filter_date=None, filter_date_month=None,
+    filter_category=None, filter_subcategory=None,
+) :
+    df = pd.read_csv(filename, sep=';')
+    for col, filter_value in [
+        ('Date opération', filter_date),
+        ('Catégorie', filter_category),
+        ('Sous-catégorie', filter_subcategory),
+    ] :
+        if not filter_value is None :
+            df = df[df[col] == filter_value]
+            
+    if not filter_date_month is None :
+        df = df[df['Date opération'].map(date2month) == filter_date_month]
+    return df
